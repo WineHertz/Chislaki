4d348f5d5c1370b2d89a6c3e7fb800e07155d2d2
winehertz <pshershakova@inbox.ru>
2021-12-22 18:02:52 +0300

    add first task

--- Task1.py
+++ Task1.py
@@ -1,33 +1,18 @@
-try:
-    import math
-except ModuleNotFoundError:
-    install("math")
-try:
-    import matplotlib.pyplot
-except ModuleNotFoundError:
-    install("matplotlib.pyplot")
-try:
-    import numpy
-except ModuleNotFoundError:
-    install("numpy")
-try:
-    import time
-except ModuleNotFoundError:
-    install("time")
-try:
-    import mpmath
-except ModuleNotFoundError:
-    install("mpmath")
-
 from math import *
-import matplotlib.pyplot as plt
-from numpy import array
-import numpy as np
-import time
 from mpmath import *
-
+import numpy as np
+mas1 = np.array([]); mas2 = np.array([]); mas3 = np.array([])
+mas4 = np.array([]); mas5 = np.array([]); mas6 = np.array([])
 
 def f(x):
+    """
+    Функция, которая возвращает значение f(x)
+    :params x: значение x
+
+    :params func: заданная функция в типе str()
+
+    :return: значение в точке x.
+    """
     try:
         a = eval(func)
     except ZeroDivisionError:
@@ -35,88 +20,107 @@ def f(x):
     return a
 
 
-def fdx(x, h):  # рассчет дифференциала от значения x с шагом
+def fdx(x, h, func):
+    """
+    Функция, которая рассчитывает значение производной в конкретной точке методом двусторонней разности.
+    :params x: значение x
+
+    :params h: шаг дифференцирования
+
+    :params func: заданная функция в типе str()
+
+    :return a: значение в дифференциале
+    """
     try:
-        func = (f(x + h) - f(x - h)) / (2 * h)
+        a = (f(x + h, func) - f(x - h, func)) / (2 * h)
     except ZeroDivisionError:
-        func = inf
-    return func
+        a = 0
+    return a
 
 
-def integ(a, b, step):  # рассчет интеграла и вывод результата
-    k = 0
-    inter = a
-    plt.title("Графики функций f(x)")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.grid()  # сеточка
-    # plt.scatter(mpf(inter),mpf(f(inter)), label = "f(x)", color='b') # легенда для графика функции
+def F(h, x, f):
+    """
+    Функция, которая рассчитывает значение интеграла в точке от x до x+шаг.
+    :params h: шаг дифференцирования
 
-    # так как мы используем метод трапеций, то будем выводить сумму значений функций в точках на промежутке
-    s = 0
-    s = float()
-    s = mpf(f(inter)) * 0.5
-    if (f(inter) != inf and f(inter) != -inf):
-        plt.scatter(mpf(inter), mpf(f(inter)), color='b')
-    print('x =', mpf(inter), 'f(x) = ', mpf(f(inter)), "s=", s)
-    inter = round((inter + step), len(str(step)))
+    :params func: заданная функция в типе str()
+
+    :params x: значение x
+
+    :return a: значение интеграла от x до (x+шаг)
+    """
+    try:
+        a = (f(h) + f(x)) * (x - h) / 2
+    except ZeroDivisionError:
+        a = 0
+    return a
+
+
+def integ(a, b, step, func):  # рассчет интеграла и вывод результата
+    """
+    Функция, рассчитывающая интеграл от a до b
+
+    :params a: нижний предел
+
+    :params b: верхний предел
+
+    :params step: шаг
+
+    :params func: заданная функция в типе str()
+
+    :return mas4: значение x
+            mas5: значение интеграла в точке от x до x+шаг
+            mas6: значение самой функции
+    """
+    inter = a + step
+    mas4 = np.arange(int((b - a) / step) + 2)
+    mas5 = np.arange(int((b - a) / step) + 2)
+    mas6 = np.arange(int((b - a) / step) + 2)
     for i in range(1, int((b - a) / step)):  # (1)
-        s = s + mpf(f(inter))
-        print('x =', mpf(inter), 'f(x) =', mpf(f(inter)), "s=", s)
-        if (f(inter) != inf and f(inter) != -inf):
-            if k == 0:
-                k = 1
-                plt.scatter(mpf(inter), mpf(f(inter)), label="f(x)", color='b')  # легенда для графика функции
-            else:
-                plt.scatter(mpf(inter), mpf(f(inter)), color='b')
-        inter = round((inter + step), len(str(step)))
-    if (f(inter) != inf and f(inter) != -inf):
-        plt.scatter(mpf(inter), mpf(f(inter)), color='b')
-    plt.legend()
-    s = s + mpf(f(inter)) * 0.5
-    print('x =', mpf(inter), 'f(x) =', mpf(f(inter)), "s=", s)
-    s = s * step
-    print("Значение интеграла", s)
-    plt.show()
-    return ('Интегрирование завершено.')
+        print('x =', round(inter, 3), 'f(x) =', round(f(inter, func), 3), "F(x) =", round(F(a, inter, func), 3))
+        mas4[i] = inter
+        mas5[i] = F(inter, step, func)
+        mas6[i] = f(inter, func)
+        inter = inter + step
+    return mas4, mas5, mas6
 
 
-def diff(a, b, step):  # рассчет дифференциала и вывод результата, когда a,b>0
-    k = 0
-    inter = a  # шаг итерации функции или как это называется я хз
-    plt.plot()
-    plt.title("Графики функций f(x), f'(x)")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.grid()  # сеточка
-    # plt.scatter(mpf(inter),mpf(fdx(inter, step)), label = "f'(x)",color = 'g') # легенда для графика производной
-    # plt.scatter(mpf(inter),mpf(f(inter)), label = "f(x)", color='b') # легенда для графика функции
-    # plt.legend()
+def diff(a, b, step, func):  # рассчет дифференциала и вывод результата, когда a,b>0
+    """
+    Функция, рассчитывающая дифференциал от a до b
+
+    :params a: нижний предел
+
+    :params b: верхний предел
+
+    :params step: шаг
+
+    :params func: заданная функция в типе str()
+
+    :return mas1: значение x
+            mas2: значение производной в точке
+            mas3: значение самой функции
+    """
+    inter = a
+    mas1 = np.arange(int((b - a) / step) + 2)
+    mas2 = np.arange(int((b - a) / step) + 2)
+    mas3 = np.arange(int((b - a) / step) + 2)
     for i in range(1, int((b - a) / step) + 2):  # (1)
-        print('x =', mpf(inter), 'f(x) =', mpf(f(inter)), "f'(x) =", mpf(fdx(inter, step)))
-        if (f(inter) != inf and f(inter) != -inf and fdx(inter, step) != inf and fdx(inter, step) != -inf):
-            if k == 0:
-                k = 1
-                plt.scatter(mpf(inter), mpf(fdx(inter, step)), label="f'(x)", color='g',
-                            s=5)  # легенда для графика производной
-                plt.scatter(mpf(inter), mpf(f(inter)), label="f(x)", color='b', s=5)  # легенда для графика функции
-            else:
-                plt.scatter(mpf(inter), mpf(fdx(inter, step)), color='g', s=5)
-                plt.scatter(mpf(inter), mpf(f(inter)), color='b', s=5)
-
-        inter = round((inter + step), len(str(step)))
-    plt.legend()
-    plt.show()
-    return ('Дифференцирование завершено.')
+        print('x =', inter, 'f(x) =', f(inter, func), "f'(x) =", fdx(inter, step, func))
+        mas1[i] = inter
+        mas2[i] = fdx(inter, step, func)
+        mas3[i] = f(inter, func)
+        inter = inter + step
+    return mas1, mas2, mas3
 
 
 func = input('Введите функцию: ')
 a = float(input('Введите нижний предел: '))
 b = float(input('Введите верхний предел: '))
-if (isinstance(f(a), mpc) == True or f(a) == -inf):
+if isinstance(f(a), mpc) == True or f(a) == -inf:
     print('Ошибка: введеные значение не удовлетворяют области значений функции. Введите функцию заново')
 else:
-    if (a > b):  # если предел a > b - это невозможно, выводим ошибку отправляем в начало.
+    if a > b:  # если предел a > b - это невозможно, выводим ошибку отправляем в начало.
         print('Ошибка ввода. Вы ввели неправильный предел. ')
     else:
         com = 'abc'
@@ -125,60 +129,38 @@ else:
                 'Введите "1" для дифференцирования, введите "2" для интегрирования.\n "0" для выхода из программы ')
             if com == '1':
                 h_o = int(input('Введите 1-шаг или 2-точность дифференцирования '))
-                if (h_o == 2):
+                if h_o == 2:
                     step = float(input("Введите точность дифференцирования"))
                     step = step ** 2
-                if (h_o == 1):
+                if h_o == 1:
                     step = float(input("Введите шаг дифференцирования"))
-                if (
-                        int(b - a) < step):  # если шаг больше, чем область дифф-ия, то тогда выводим ошибку и отправляем в начало
+                if int(b - a) < step:
                     print('Ошибка ввода, шаг дифференцирования не может быть больше области дифференцирования.')
                 else:  # если шаг меньше, и все хорошо, тогда выводим результат и забываем о существовании программы.
-
                     print(diff(a, b, step))
                     break
                 break
             if com == '2':
-                h_o = int(input('Введите 1-шаг или 2-точность интегрировния'))
-                if (h_o == 2):
-                    step = float(input("Введите точность интегрировния"))
+                h_o = int(input('Введите 1-шаг или 2-точность интегрирования'))
+                if h_o == 2:
+                    step = float(input("Введите точность интегрирования"))
                     step = step ** 2
-                if (h_o == 1):
-                    step = float(input("Введите шаг интегрировния"))
-                if (
-                        int(b - a) < step):  # если шаг больше, чем область дифф-ия, то тогда выводим ошибку и отправляем в начало
+                if h_o == 1:
+                    step = float(input("Введите шаг интегрирования"))
+                if int(b - a) < step:
                     print('Ошибка ввода, шаг дифференцирования не может быть больше области интегрирования .')
                 else:  # если шаг меньше, и все хорошо, тогда выводим результат и забываем о существовании программы.
-
                     print(integ(a, b, step))
                     break
                 break
             else:
                 print('Введена неправильная команда.')
 
-#вычисление времени работы функций в точке а (дифференцирование)
-try:
-    import scipy.misc
-except ModuleNotFoundError:
-    install("scipy.misc")
-from scipy.misc import derivative
-print("Для встроенной функции:")
-start = time.perf_counter_ns()
-derivative(f,a)
-end = time.perf_counter_ns()
-print(f"Время выполнения = {end - start}")
-print("Для написанной функции:")
-start = time.perf_counter_ns()
-fdx(a, step)
-end = time.perf_counter_ns()
-print(f"Время выполнения = {end - start}")
-
-
-def trap():
-    s=0.5*(f(a)+f(b))
-    x=a+step
+def method_trap():
+    s = 0.5*(f(a)+f(b))
+    x = a+step
     while x <= b - step:
         s += f(x)
         x += step
-    s=s*step
+    s = s*step
     return s
